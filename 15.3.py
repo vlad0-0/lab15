41ee48b12436b19d5f23635cafedd6ccd4e1f7a6
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-12-03 13:38:37 +0300

    Update 15.3.py

--- 15.3.py
+++ 15.3.py
@@ -3,11 +3,11 @@ a = []
 for i in range(n):
     a.append(int(input()))
 b = 0
+for i in range(n-1, -1, -1):
+    if a[i]%2 == 1:
+        b = a[i]
+        break
 for i in range(n):
     if a[i]%2 == 1:
-        if b == 0:
-            b = a[i]
-        elif b != 0:
-            a[i] += b
-            b = a[i]-b
+        a[i] += b
 print(a)
